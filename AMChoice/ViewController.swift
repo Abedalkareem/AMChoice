851edd92c95471c75437ed890ce725c76e2991e2
Abedalkareem <abdalkareem.omarieh@sdvision.net>
2017-05-07 11:28:56 +0300

    Initial Commit

---
+++ AMChoice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AMChoice
+//
+//  Created by abedalkareem omreyh on 5/7/17.
+//  Copyright © 2017 abedalkareem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
